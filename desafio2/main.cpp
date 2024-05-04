f005a4588c1eb4617e6cb8dfc5c673edfc576474
SalomeBM <125213769+SalomeBM@users.noreply.github.com>
2024-05-04 12:54:09 -0500

    Creación de programa y clases principales

---
+++ desafio2/main.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Red {
+private:
+    Linea** lineas;
+    int capacidad;
+    int numLineas;
+
+public:
+    Red(int _capacidad = 5) : capacidad(_capacidad), numLineas(0) {
+
+    }
+
+    ~Red() {
+
+    }
+
+    int numeroLineas() const {
+
+    }
+
+    void agregarLinea(Linea* linea) {
+
+    }
+
+    bool eliminarLinea(Linea* linea) {
+
+    }
+
+    int numeroEstaciones() const {
+
+    }
+};
+
+class Linea {
+private:
+    string nombre;
+    string transporte;
+    Estacion** estaciones;
+    int capacidad;
+    int numEstaciones;
+
+public:
+    Linea(string _nombre, string _transporte, int _capacidad = 10) : nombre(_nombre), transporte(_transporte), capacidad(_capacidad), numEstaciones(0) {
+
+    }
+
+    ~Linea() {
+
+    }
+
+    string getNombre() const {
+
+    }
+
+    int numeroEstaciones() const {
+
+    }
+
+    void agregarEstacion(Estacion* estacion) {
+
+    }
+
+    bool eliminarEstacion(Estacion* estacion) {
+
+    }
+
+    bool tieneEstacion(Estacion* estacion) const {
+
+    }
+};
+
+class Estacion {
+private:
+    string nombre;
+    int tiempoAnterior;
+    int tiempoSiguiente;
+    bool transferencia;
+
+public:
+    Estacion(string _nombre, bool _transferencia) : nombre(_nombre), transferencia(_transferencia) {}
+
+    string getNombre() const {
+        return nombre;
+    }
+
+    bool esTransferencia() const {
+        return transferencia;
+    }
+};
+
+int main() {
+
+
+    return 0;
+}
